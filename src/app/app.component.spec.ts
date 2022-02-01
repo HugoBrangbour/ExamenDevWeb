6e4a666f74b22c3b7688af6d5aa026007916d0fe
Hugo BRANGBOUR <brangbour.hugo@gmail.com>
2022-02-01 11:29:37 +0100

    Init 2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ExamenDevWeb'`, () => {
+  it(`should have as title 'cours-dev-web-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ExamenDevWeb');
+    expect(app.title).toEqual('cours-dev-web-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ExamenDevWeb app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cours-dev-web-angular app is running!');
   });
 });
